1621cebabc2619b518f318c35261758da10aae98
Chanv Kuldeep <93032744+kuljitchauv@users.noreply.github.com>
2022-07-20 15:55:02 +0530

    Add files via upload

---
+++ Unit-2/P4.java
@@ -0,0 +1,25 @@
+//4.
+
+import java.util.Scanner;
+
+class H1
+{
+  H1 (int i, float j)
+  {
+    System.out.println ("A Addition of I & J :" + (i + j));
+  }
+}
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner input = new Scanner (System.in);
+
+      System.out.println ("Enter Value of I :");
+    int may = input.nextInt ();
+      System.out.println ("Enter Value of J :");
+    float Ja = input.nextFloat ();
+    H1 S = new H1 (may, Ja);
+  }
+}
---
+++ Unit-2/P5.java
@@ -0,0 +1,31 @@
+//5.
+
+import java.util.Scanner;
+
+class H1
+{
+  int i;
+  float j;
+
+    H1 (int i, float j)
+  {
+    this.i = i;
+    this.j = j;
+    
+    System.out.println("A Addition of i & j :"+(i+j));
+  }
+}
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner input = new Scanner (System.in);
+
+    System.out.println ("Enter Value of I :");
+    int mai = input.nextInt ();
+    System.out.println ("Enter Value of J :");
+    float ja = input.nextFloat ();
+    H1 S = new H1 (mai,ja);
+  }
+}
