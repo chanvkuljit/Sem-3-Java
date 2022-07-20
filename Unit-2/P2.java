5db4b037983b0f041ddeaaf42a9484ffdaf27313
Chanv Kuldeep <93032744+kuljitchauv@users.noreply.github.com>
2022-07-20 15:51:46 +0530

    Create P2.java

---
+++ Unit-2/P2.java
@@ -0,0 +1,23 @@
+//2.
+
+import java.util.Scanner;
+
+public class P2
+{
+  public static void main (String[]args)
+  {
+    Scanner input = new Scanner (System.in);
+
+      System.out.println ("Enter Value Of A :");
+    int a = input.nextInt ();
+      System.out.println ("Enter Value of B :");
+    int b = input.nextInt ();
+
+      Addition (a, b);
+  }
+
+  static void Addition (int a, int b)
+  {
+    System.out.println ("A Addition of A & B is :" + (a + b));
+  }
+}
