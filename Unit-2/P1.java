58e05ca3e5af08794c388403b23ae13bcb0e9923
Chanv Kuldeep <93032744+kuljitchauv@users.noreply.github.com>
2022-07-20 15:50:10 +0530

    Create P1.java

---
+++ Unit-2/P1.java
@@ -0,0 +1,26 @@
+//1.
+
+class Greeting
+{
+  public static void Hello ()
+  {
+    System.out.println ("How are You!!");
+  }
+}
+
+
+
+public class P1
+{
+  public static void Hello ()
+  {
+    System.out.println ("Hello Guy!!");
+  }
+
+  public static void main (String[]args)
+  {
+    Hello ();
+    Greeting S1 = new Greeting ();
+    S1.Hello ();
+  }
+}
