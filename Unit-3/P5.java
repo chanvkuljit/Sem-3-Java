d8777e7e1c90ac9cfc517da6a9c62a3f53cc0fe8
Chanv Kuldeep <93032744+kuljitchauv@users.noreply.github.com>
2022-09-01 14:38:44 +0530

    Create P5.java

---
+++ Unit-3/P5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class first
+{
+	int num=1;
+
+}
+
+class second extends first
+{
+	int num=2;
+
+	void display()
+	{
+	
+	    System.out.println(num);
+	    System.out.println(super.num);
+	}
+}
+
+public class P5
+{
+	public static void main(String[] args)
+	{
+		second s = new second();
+
+		s.display();
+	}
+}
